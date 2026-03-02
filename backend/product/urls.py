4bed44fe6bac4dfce117a77a774e5a6b784ac628
SaikhanK <saikhan.khasukhanov@gmail.com>
2026-03-02 23:09:25 +0100

    update product api

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .view import ProductViewSet, ProductImageViewSet
+from .view import ProductViewSet, ProductImageViewSet, ProductView
 from .test_api import *
 from django.conf import settings
 from django.conf.urls.static import static
@@ -13,4 +13,5 @@ router.register(r"image", ProductImageViewSet, basename="images")
 urlpatterns = [
     path('', include(router.urls)),
     path('test/', TestViewSet.as_view({'get': 'product_test'}), name="test_api"),
+    path('testa/', ProductView.as_view({'get': 'getProducts'}), name="product")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/product/view.py
+++ backend/product/view.py
@@ -1,6 +1,23 @@
+from rest_framework.viewsets import GenericViewSet
 from rest_framework import viewsets
+from rest_framework.request import Request
+from rest_framework.response import Response
 from .models import Product, ProductImage
 from .serializers import ProductSerializer, ProductImageSerializer
+from .paramter import QueryParameter
+import pandas as pd
+
+class ProductView(GenericViewSet):
+
+    def getProducts(self, request: Request) -> Response:
+        data = QueryParameter.model_validate(request.query_params)
+        product = Product.objects.filter(
+            attributes__attribute__name="Color",
+            attributes__value__value="Black"
+        ).distinct()
+        df = pd.DataFrame(product)
+        raise ValueError(df)
+        return Response
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
